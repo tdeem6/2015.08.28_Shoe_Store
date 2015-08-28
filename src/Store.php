ce9ca82d8317ef2e3f99d7e32bf77db07de108bf
Tyler Deem <tdeem6@gmail.com>
2015-08-28 11:02:52 -0700

    Add store.php with functions to be tested.

---
+++ src/Store.php
@@ -0,0 +1,79 @@
+<?php
+
+    class Store
+    {
+
+        private $store_name;
+        private $id;
+
+        function __construct($store_name, $id = null)
+        {
+            $this->store_name = $store_name;
+            $this->id = $id;
+        }
+
+        function setStoreName($new_store_name)
+        {
+            $this->store_name = (string) $new_store_name;
+        }
+
+        function getStoreName()
+        {
+            return $this->store_name;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO stores (store_name) VALUES ('{$this->getStoreName()}')");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        static function getAll()
+        {
+            $returned_stores = $GLOBALS['DB']->query("SELECT * FROM stores;");
+            $stores = array();
+            foreach($returned_stores as $store) {
+              $store_name = $store['store_name'];
+              $id = $store['id'];
+              $new_store = new Store($store_name, $id);
+              array_push($stores, $new_store);
+            }
+            return $stores;
+        }
+
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM stores;");
+        }
+
+        static function find($search_id)
+        {
+            $found_store = null;
+            $stores = Store::getAll();
+            foreach($stores as $store) {
+                $store_id = $store->getId();
+                if ($store_id == $search_id) {
+                    $found_store = $store;
+                }
+            }
+            return $found_store;
+        }
+
+        function update($new_store_name)
+        {
+            $GLOBALS['DB']->exec("UPDATE stores SET name = '{$new_store_name}' WHERE id = {$this->getId()};");
+            $this->setStoreName($new_store_name);
+        }
+
+        function delete()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM stores WHERE id = {$this->getId()};");
+            $GLOBALS['DB']->exec("DELETE FROM brands WHERE store_id = {$this->getId()};");
+        }
+    }
+?>
